b1d1c88e1c7303bacc5b2d19bf32fb382dd8ffaa
srivastavaarachit <rachitsrivastava2005@gmail.com>
2025-10-28 11:34:55 -0700

    Update testcolorsensor.m

--- testcolorsensor.m
+++ testcolorsensor.m
@@ -1,11 +1,23 @@
-brick.SetColorMode(1, 4); % Set Color Sensor connected to Port 1 to Color Code Mode
+brick.SetColorMode(1, 4); % Set Color Sensor connected to Port 1 to RGB Mode
+color_rgb = brick.ColorRGB(1); % Get Color on port 1
 
-color_rgb = brick.ColorRGB(1); % Get Color on port 1.
+% Determine which color stop is detected
+red_val = color_rgb(1);
+green_val = color_rgb(2);
+blue_val = color_rgb(3);
 
-%print color of object
+% Identify the color based on RGB values
+if (green_val > red_val) && (green_val > blue_val) && (green_val > 100)
+    fprintf("Color Detected: YELLOW\n");
+elseif (green_val > red_val) && (green_val > blue_val) && (green_val < 100)
+    fprintf("Color Detected: GREEN\n");
+elseif (blue_val > red_val) && (blue_val > green_val)
+    fprintf("Color Detected: BLUE\n");
+else
+    fprintf("Color Detected: UNKNOWN\n");
+end
 
-fprintf("\tRed: %d\n", color_rgb(1));
-
-fprintf("\tGreen: %d\n", color_rgb(2));
-
-fprintf("\tBlue: %d\n", color_rgb(3));
+% Display raw RGB values for reference
+fprintf("\tRed: %d\n", red_val);
+fprintf("\tGreen: %d\n", green_val);
+fprintf("\tBlue: %d\n", blue_val);
