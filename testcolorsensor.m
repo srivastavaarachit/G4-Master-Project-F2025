c90512f9e9d0c19a648b74c7eccbaa300e4a8e4e
srivastavaarachit <rachitsrivastava2005@gmail.com>
2025-10-28 12:08:36 -0700

    Update testcolorsensor.m

--- testcolorsensor.m
+++ testcolorsensor.m
@@ -1,5 +1,5 @@
-brick.SetColorMode(1, 4); 
-color_rgb = brick.ColorRGB(1); 
+brick.SetColorMode(1, 4); % Set Color Sensor connected to Port 1 to RGB Mode
+color_rgb = brick.ColorRGB(1); % Get Color on port 1
 
 % Get RGB values
 red_val = color_rgb(1);
@@ -7,11 +7,11 @@ green_val = color_rgb(2);
 blue_val = color_rgb(3);
 
 % Identify the color based on RGB values
-if (red_val > 100) && (green_val > 80) && (blue_val < 50)
-    % Yellow has both red and green high, blue low
+if (red_val > green_val) && (green_val > 40) && (blue_val < 30) && (red_val > 60)
+    % Yellow has red higher than green, both much higher than blue
     fprintf("Color Detected: YELLOW\n");
-elseif (green_val > red_val) && (green_val > blue_val) && (blue_val < 50)
-    % Green has green dominant, blue low
+elseif (green_val > red_val) && (green_val > blue_val) && (green_val > 20)
+    % Green has green dominant (lowered threshold from 50 to 20)
     fprintf("Color Detected: GREEN\n");
 elseif (blue_val >= green_val) && (blue_val > red_val)
     % Blue has blue dominant
@@ -20,7 +20,7 @@ else
     fprintf("Color Detected: UNKNOWN\n");
 end
 
-% Display raw RGB values for reference testing
+% Display raw RGB values for reference
 fprintf("\tRed: %d\n", red_val);
 fprintf("\tGreen: %d\n", green_val);
 fprintf("\tBlue: %d\n", blue_val);
